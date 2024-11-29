8eb71c180da27482821b0a450916e9c176f76c54
Nadine <nmager265@gmail.com>
2024-11-29 18:44:16 +0100

    initialCommit ASP.NET

---
+++ WebApplication1/WebApplication1/Controllers/EmployeeController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Data;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public EmployeeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        
+        public IActionResult Index()
+        {
+            var employees = _context.Employees.ToList();
+            ViewBag.Employees = employees;
+            return View();
+        }
+
+        public IActionResult CreateEdit(int id)
+        {
+            if(id == 0)
+            {
+                return View("CreateEditEmployee");
+            }
+            
+            var employee = _context.Employees.Find(id);
+
+            if(employee == null)
+            {
+                return NotFound();
+            }
+
+            return View("CreateEditEmployee", employee);
+        }
+
+        [HttpPost]
+        public IActionResult CreateEditEmployee(Employee employee)
+        {
+            if(employee.ID == 0)
+            {
+                _context.Employees.Add(employee);
+            }
+            else
+            {
+                _context.Employees.Update(employee);
+            }
+            
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult DeleteEmployee(int id)
+        {
+            var employee = _context.Employees.Find(id);
+
+            if(employee == null)
+            {
+                return NotFound();
+            }
+            
+            _context.Employees.Remove(employee);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication1.Models
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string Prename {  get; set; }
+        public string Lastname { get; set; }
+
+    }
+}
