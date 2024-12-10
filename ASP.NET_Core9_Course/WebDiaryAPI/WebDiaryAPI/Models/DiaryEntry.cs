85524e8156ac7a3a2ed540c7c77302185ddaf5ae
Nadine <nmager265@gmail.com>
2024-12-10 20:04:03 +0100

    Kapitel61

---
+++ ASP.NET_Core9_Course/WebDiaryAPI/WebDiaryAPI/Models/DiaryEntry.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebDiaryAPI.Models
+{
+    public class DiaryEntry
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(100, MinimumLength = 3)]
+        public string Title { get; set; } = string.Empty;
+        [Required]
+        public string Content { get; set; } = string.Empty;
+        [Required]
+        public DateTime Created { get; set; } = DateTime.Today;
+    }
+}
